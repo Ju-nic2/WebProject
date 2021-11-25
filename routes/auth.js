422a2bcf52e0689d8678ec01a12f41871fa72579
Ju-nic2 <ata97@naver.com>
2021-11-25 19:50:49 +0900

    add : menu

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/login', (req, res, next) => {
-    res.send('로그인 성공!');
+    res.redirect('/home')
   });
 
 
--- routes/page.js
+++ routes/page.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'JunicWorld' });
+    res.render('index', { title: 'JunicWorld',isLogined :false });
   });
+
+router.get('/home', (req, res, next) => {
+    res.render('home', { title: 'JunicWorld',isLogined : true, where : 'home'});
+});
 
 router.get('/signup', (req, res, next) => {
     res.send('회원가입');
